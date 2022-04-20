e889e613c469ab1102edfa998e8f988c7c722fc3
psdCcc <86864836+psdCcc@users.noreply.github.com>
2022-04-20 04:04:08 +0100

    Update main.cpp

--- Source/Embedded-part/main.cpp
+++ Source/Embedded-part/main.cpp
@@ -1,24 +1,30 @@
-#include "./DHT11/DHT11.h"
-#include "./HC_SR501/HC_SR501.h"
-#include "./Rain Sensor/RAIN_SENSOR.h"
-#include "./Camera/Camera.h"
+#include "DHT11.h"
+#include "Camera.h"
+#include <sys/wait.h>
 
 int main(int argc, char* argv[]){
-    DHT11 dht11;
-    HC_SR501 hc_sr501;
-    RAIN rain;
-    Camera camera;
     //initialization of pigpio library
     if (gpioInitialise() < 0)
 		std::cout << "GPIO Initialising failed/n" <<std::endl;
 	else {
-        //initializtion of the modules
-        dht11.init();
-        hc_sr501.init();
-        rain.init();
-        camera.init();
+        dht11_para dht11_p;
+        camera_para camera_p;
+        DHT11 dht11;
+        Camera camera;
+        dht11_p.dht11 = &dht11;
+        camera_p.camera = &camera;
+        camera_p.port = 9999;
+        pid_t c, w;
+        c = fork();
+        if (c == 0){
+            camera.start(&camera_p);
+        }
+        else{
+            dht11.start(&dht11_p);
+            w = waitpid(c, nullptr, 0);
+        }
     }
     
-    
+    gpioTerminate();
     return 0;
 }
