17f50df4427cab528227dd172e370b9fdffb95fe
psdCcc <86864836+psdCcc@users.noreply.github.com>
2022-04-20 04:05:10 +0100

    Update Camera.h

--- Source/Embedded-part/Camera/Camera.h
+++ Source/Embedded-part/Camera/Camera.h
@@ -7,10 +7,21 @@
 #include <string>
 #include <unistd.h>
 
+using std::string;
+using std::stoi;
+using std::to_string;
+using std::cout;
+using std::endl;
+
 class Camera{
 public:
-    uint8_t init(int port);// Function uint8_t init(int port): create a stream server. Return 0 if succeeds or 1 if failes.
-    uint8_t sendIPAddr(std::string serverIP);// Function uint8_t sendIPAddr(string serverIP): Send the IP address of raspberry pi to cloud server. Return 0 if succeeds or 1 if failes.
+    uint8_t init(void* port);// Function uint8_t init(void* port): create a stream server. Return 0 if succeeds or 1 if failes.
+    static void* start(void* args);
+};
+
+struct camera_para{
+    int port;//port of the stream
+    Camera* camera;//Pointer points to a camera object
 };
 
 #endif
