723ae9773d7c78284c59eb683677c5331066208f
psdCcc <86864836+psdCcc@users.noreply.github.com>
2022-04-20 04:04:41 +0100

    Update Camera.cpp

--- Source/Embedded-part/Camera/Camera.cpp
+++ Source/Embedded-part/Camera/Camera.cpp
@@ -1,16 +1,20 @@
 #include "Camera.h"
 
-uint8_t Camera::init(int port){
-    std::string tcpIP = "tcp://0.0.0.0:" + std::to_string(port);
+uint8_t Camera::init(void* Port){
+    int port = *(int*) Port;
+    string tcpIP = "tcp://0.0.0.0:" + to_string(port);
     if (execlp("libcamera-vid", "libcamera-vid", "-t", "0", "--inline", "--listen", "-o", 
     tcpIP.c_str(), nullptr) == -1){
-        std::cout << "Initialization failed." << std::endl;
+        cout << "Initialization failed." << endl;
         return 1;
     }
-    std::cout << "Initialization succeeded." << std::endl;
+    cout << "Initialization succeeded." << endl;
     return 0;
 }
 
-uint8_t Camera::sendIPAddr(std::string serverIP){
-    return 0;
+void* Camera::start(void* args){
+    camera_para para = *(camera_para*) args;
+    para.camera->init(&para.port);
+    
+    return (void*) 1;
 }
